b38a45be71f66f67ae99f0dce188f8edeb56c6df
Lord Mike <lordmike@iki.fi>
2019-09-01 16:27:20 +0300

    Added init.bat

---
+++ init.bat
@@ -0,0 +1,19 @@
+::C:\Windows\SysWOW64\cmd.exe
+@echo off
+
+:: Copyright (c) 2019, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+:: 
+:: License of this script file:
+::   MIT License
+:: 
+:: License is available online:
+::   https://github.com/lordmikefin/kvm-gpu-guide/blob/master/LICENSE.rst
+:: 
+:: Latest version of this script file:
+::   https://github.com/lordmikefin/kvm-gpu-guide/blob/master/init.bat
+
+:: init.bat
+:: This script will install software for my other scripts :)
+:: Download and install.
+::  - Git
+::  - Python

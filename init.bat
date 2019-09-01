a7a3a4a6b40614080530f8217ecb005d85382526
Lord Mike <lordmike@iki.fi>
2019-09-01 16:38:22 +0300

    TODOs for init

--- init.bat
+++ init.bat
@@ -17,3 +17,26 @@
 :: Download and install.
 ::  - Git
 ::  - Python
+
+
+:: TODO: Create directory for Installer and install path.
+::   C:\LM_ToyBox\temp
+::   C:\LM_ToyBox\apps
+::   C:\LM_ToyBox\apps\Git
+::   C:\LM_ToyBox\apps\Python\Python37
+
+
+
+:: TODO: Download with PowerShell
+::   https://superuser.com/questions/25538/how-to-download-files-from-command-line-in-windows-like-wget-or-curl
+
+
+:: Download Git into temp folder
+::   https://git-scm.com/download/win
+::   https://github.com/git-for-windows/git/releases/download/v2.23.0.windows.1/Git-2.23.0-64-bit.exe
+
+:: Download Python into temp folder
+::   https://www.python.org/downloads/windows/
+::   https://www.python.org/ftp/python/3.7.4/python-3.7.4-amd64.exe
+
+

d76a04c1ca065708863eb8bb7c6d11dbd6ee8455
Lord Mike <lordmike@iki.fi>
2019-09-02 01:17:06 +0300

    Do not download if file already exists.

--- init.ps1
+++ init.ps1
@@ -40,32 +40,46 @@ Write-Host "Current_script_ver: ${Current_script_ver} (${Current_script_date})"
 #   https://superuser.com/questions/25538/how-to-download-files-from-command-line-in-windows-like-wget-or-curl
 #$client = new-object System.Net.WebClient
 #$client.DownloadFile("http://www.xyz.net/file.txt","C:\tmp\file.txt")
+$client = new-object System.Net.WebClient
 
 
 
-# TODO: Before downloading check if file already exists.
-#       Do not download again if we have the file!
+# TODO: How to suppress the 'Get-Item' error?
 
 
 # Download Git into temp folder
+Write-Host ""
 $url = "${Env:URL_GIT}"
 $file = "${Env:PATH_INSTALLERS}\${Env:FILE_GIT}"
-Write-Host ""
+$file_size = (Get-Item $file).length
 Write-Host "Download Git into temp folder"
 Write-Host "url: ${url}"
 Write-Host "file: ${file}"
-$client = new-object System.Net.WebClient
-$client.DownloadFile("${url}","${file}")
+#Write-Host "file size: ${file_size}"
+#file size: 47701816
+if (47701816) {
+	Write-Host "File already exist."
+} else {
+	$client.DownloadFile("${url}","${file}")
+}
+
 
 # Download Python into temp folder
+Write-Host ""
 $url = "${Env:URL_PY37}"
 $file = "${Env:PATH_INSTALLERS}\${Env:FILE_PY37}"
-Write-Host ""
+$file_item = Get-Item $file
+$file_size = ${file_item}.length
 Write-Host "Download Git into temp folder"
 Write-Host "url: ${url}"
 Write-Host "file: ${file}"
-$client = new-object System.Net.WebClient
-$client.DownloadFile("${url}","${file}")
+#Write-Host "file size: ${file_size}"
+#file size: 26680368
+if ( ${file_size} -eq 26680368 ) {
+	Write-Host "File already exist."
+} else {
+	$client.DownloadFile("${url}","${file}")
+}
 
 
 Write-Host ""

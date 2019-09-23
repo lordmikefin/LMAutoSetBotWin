2fe3bc76f8bdd29a560acf052984079f733232bd
Lord Mike <lordmike@iki.fi>
2019-09-23 15:58:36 +0300

    Initial batch script for launching the python script :)

---
+++ setup_apps.bat
@@ -0,0 +1,31 @@
+::C:\Windows\SysWOW64\cmd.exe
+@echo off
+
+:: Copyright (c) 2019, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+:: 
+:: License of this script file:
+::   MIT License
+:: 
+:: License is available online:
+::   https://github.com/lordmikefin/LMAutoSetBotWin/blob/master/LICENSE
+:: 
+:: Latest version of this script file:
+::   https://github.com/lordmikefin/LMAutoSetBotWin/blob/master/setup_apps.bat
+
+:: setup_apps.bat
+:: This script will start python script.
+:: 
+:: The python script will download and install.
+::  - Notepad++
+
+SET CURRENT_SCRIPT_VER=0.0.1
+SET CURRENT_SCRIPT_DATE=2019-09-23
+SET CURRENT_SCRIPT=setup_apps.bat
+echo CURRENT_SCRIPT_VER: %CURRENT_SCRIPT_VER% (%CURRENT_SCRIPT_DATE%)
+
+:: TODO: Activate python virtual environment 'venv-LMAutoSetBotWin'.
+
+:: TODO: Run the python script :)
+
+echo.
+echo End of script '%CURRENT_SCRIPT%'

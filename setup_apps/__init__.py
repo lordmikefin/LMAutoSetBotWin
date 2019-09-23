837693b61a77487db554994c43044c17de55c2a8
Lord Mike <lordmike@iki.fi>
2019-09-23 14:38:16 +0300

    Created initial python module for installing apps.

---
+++ setup_apps/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+"""
+    setup_apps
+    ~~~~~~~~~~
+
+    Module for installing windows applictions.
+
+    License of this script file:
+       MIT License
+
+    License is available online:
+      https://github.com/lordmikefin/LMAutoSetBotWin/blob/master/LICENSE
+
+    Latest version of this script file:
+      https://github.com/lordmikefin/LMAutoSetBotWin/blob/master/setup_apps/__init__.py
+
+
+    :copyright: (c) 2019, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+    :license: MIT License
+"""
---
+++ setup_apps/npp.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+    setup_apps
+    ~~~~~~~~~~
+
+    Install Notepad++
+
+    License of this script file:
+       MIT License
+
+    License is available online:
+      https://github.com/lordmikefin/LMAutoSetBotWin/blob/master/LICENSE
+
+    Latest version of this script file:
+      https://github.com/lordmikefin/LMAutoSetBotWin/blob/master/setup_apps/npp.py
+
+
+    :copyright: (c) 2019, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+    :license: MIT License
+"""
+
+# TODO: Download Notepad++ installer
+
+# TODO: Install Notepad++

89a52d6d158ec2967e1653398a404e9e24396d49
Lord Mike <lordmike@iki.fi>
2019-09-30 11:25:13 +0300

    Created python function from test code.

--- setup_apps.py
+++ setup_apps.py
@@ -27,7 +27,7 @@
 #__docformat__ = 'reStructuredText'
 
 __license__ = "MIT License"
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __revision__ = "setup_apps.py  v" + __version__ + " (2019-09-30)"
 
 import sys
@@ -41,6 +41,21 @@ def print_python_enviroment_info():
 	print('Python version : ' + str(sys.version))
 	print('sys.prefix     : ' + str(sys.prefix))
 	print('')
+
+
+def activate_virtual_environment():
+	""" Load Python virtual environment. """
+	# http://virtualenvwrapper.readthedocs.io/en/latest/command_ref.html
+	#os.system('lsvirtualenv')
+
+	#https://docs.python.org/3/tutorial/venv.html
+	#py_home = '/var/www/venv-lm-scripts/env'
+	#activate_this = py_home + '/bin/activate_this.py'
+	py_home = 'C:/Users/lordmike/Envs/venv-LMAutoSetBotWin'
+	activate_this = py_home + '/Scripts/activate_this.py'
+
+	with open(activate_this) as file_:
+		exec(file_.read(), dict(__file__=activate_this))
 
 
 # https://stackoverflow.com/questions/7791574/how-can-i-print-a-python-files-docstring-when-executing-it
@@ -69,31 +84,11 @@ if __name__ == '__main__':
 	#print(args.myFilenameVariable)
 
 	print_python_enviroment_info()
+	activate_virtual_environment()
+	print_python_enviroment_info()
 
-
-# TODO: Create functions from these test codes :)
-
-
-
-
-
-
-# http://virtualenvwrapper.readthedocs.io/en/latest/command_ref.html
-#os.system('lsvirtualenv')
-
-#https://docs.python.org/3/tutorial/venv.html
-#py_home = '/var/www/venv-lm-scripts/env'
-#activate_this = py_home + '/bin/activate_this.py'
-py_home = 'C:/Users/lordmike/Envs/venv-LMAutoSetBotWin'
-activate_this = py_home + '/Scripts/activate_this.py'
-
-with open(activate_this) as file_:
-	exec(file_.read(), dict(__file__=activate_this))
-
-print_python_enviroment_info()
-
-
-
-import setup_apps
+	# TODO: Import at top of this script.
+	# TODO: Activat 'setup_apps' module with function call !
+	import setup_apps
 
 

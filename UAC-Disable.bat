036ab43608cf6047a23dafa93a14810ea21687a0
Lord Mike <lordmike@iki.fi>
2019-10-06 15:01:50 +0300

    Disable UAC command. No access :(

--- UAC-Disable.bat
+++ UAC-Disable.bat
@@ -15,21 +15,37 @@
 :: UAC-Disable.bat
 :: This script will disable windows UAC
 
-SET CURRENT_SCRIPT_VER=0.0.1
+SET CURRENT_SCRIPT_VER=0.0.2
 SET CURRENT_SCRIPT_DATE=2019-10-06
 SET CURRENT_SCRIPT=UAC-Disable.bat
 echo CURRENT_SCRIPT_VER: %CURRENT_SCRIPT_VER% (%CURRENT_SCRIPT_DATE%)
 
+::SET CMD_APP=C:\Windows\System32\cmd.exe
+SET CMD_APP=%windir%\System32\cmd.exe
+SET REC_APP=%windir%\System32\reg.exe
+
 :: https://www.howtogeek.com/howto/windows-vista/enable-or-disable-uac-from-the-windows-vista-command-line/
 :: https://www.howtogeek.com/howto/windows-vista/disable-user-account-control-uac-the-easy-way-on-windows-vista/
 
-:: TODO: Tell user about disabling UAC. And give link to guide "Disable User Account Control (UAC) the Easy Way on Win 7, 8, or 10"
+:: Tell user about disabling UAC. And give link to guide "Disable User Account Control (UAC) the Easy Way on Win 7, 8, or 10"
+echo.
+echo I will disable User Account Control (UAC).
+echo This can be done manually from GUI:
+echo   https://www.howtogeek.com/howto/windows-vista/disable-user-account-control-uac-the-easy-way-on-windows-vista/
 
-:: TODO: Ask permission
+:: Ask permission
+echo.
+echo Do you want to disable the UAC.
+echo   Interupt with Ctrl + c
+pause
 
-:: TODO: Disable the UAC
+:: Disable the UAC
+:: C:\Windows\System32\cmd.exe /k %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+%REC_APP% ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+::%CMD_APP% /k %REC_APP% ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 
-:: TODO: After you enable or disable UAC, you will have to reboot your computer for the changes to take effect.
+echo.
+echo After you enable or disable UAC, you will have to reboot your computer for the changes to take effect.
 
 echo.
 echo End of script '%CURRENT_SCRIPT%'

b8d575d681b0f1b1a047ac6d51df22798b7a789e
Lord Mike <lordmike@iki.fi>
2019-10-07 11:00:57 +0300

    Initial Java install module

--- setup_apps/java.py
+++ setup_apps/java.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF-8 -*-
 """
-	pydev.py
-	~~~~~~~~
+	java.py
+	~~~~~~~
 
-	Install PyDev a Python IDE for Eclipse.
+	Install Java JDK/JRE.
 
 	License of this script file:
 	   MIT License
@@ -62,33 +62,3 @@ def install_jdk():
 	https://download.java.net/java/GA/jdk13/5b8a42f3905b406298b72d750b6919f6/33/GPL/openjdk-13_windows-x64_bin.zip.sha256
 	'''
 	pass
-
-
-def is_installed_eclipse():
-	False
-
-	
-def install_eclipse():
-	'''
-	TODO: download and install Eclipse
-	https://www.eclipse.org/downloads/
-	https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2019-09/R/eclipse-inst-win64.exe
-	https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2019-09/R/eclipse-inst-win64.exe&mirror_id=1099
-	'''
-	pass
-
-
-def is_installed_pydev():
-	False
-
-	
-def install_pydev():
-	'''
-	TODO: download pydev and extract into Eclipse
-	https://www.pydev.org/
-	https://www.pydev.org/manual_101_root.html
-	https://www.pydev.org/manual_101_install.html
-	https://www.pydev.org/manual_101_interpreter.html
-	'''
-	pass
-

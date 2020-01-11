6245cfa749512cd478d972a9304d182eebe076de
Lord Mike <lordmike@iki.fi>
2020-01-11 17:10:59 +0200

    simple XML file write test

--- setup_apps/config.py
+++ setup_apps/config.py
@@ -19,3 +19,26 @@
     :license: MIT License
 """
 
+import xml.etree.ElementTree as ET
+
+# https://stackabuse.com/reading-and-writing-xml-files-in-python/
+
+def create_test_xml():
+    # create the file structure
+    print('create the XML file structure')
+    xml_config = ET.Element('test_element')
+    items = ET.SubElement(xml_config, 'items')
+    item1 = ET.SubElement(items, 'item')
+    item2 = ET.SubElement(items, 'item')
+    item1.set('name','item1')
+    item2.set('name','item2')
+    item1.text = 'item1abc'
+    item2.text = 'item2abc'
+
+    # create a new XML file with the results
+    print('create a new XML file with the results')
+    mydata = str(ET.tostring(xml_config),'utf-8')
+    print('mydata type: ' + str(type(mydata)))
+    myfile = open("xml_config.xml", "w")
+    myfile.write(mydata)
+    myfile.close()

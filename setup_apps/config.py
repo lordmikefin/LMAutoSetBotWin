0c93513030bb54061b16b332cca428938cbf9918
Lord Mike <lordmike@iki.fi>
2020-01-11 16:47:13 +0200

    Initial XML configuration file handler

---
+++ setup_apps/config.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+"""
+    config.py
+    ~~~~~~~~~
+
+    XML configuration file handler.
+
+    License of this script file:
+       MIT License
+
+    License is available online:
+      https://github.com/lordmikefin/LMAutoSetBotWin/blob/master/LICENSE
+
+    Latest version of this script file:
+      https://github.com/lordmikefin/LMAutoSetBotWin/blob/master/setup_apps/config.py
+
+
+    :copyright: (c) 2020, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+    :license: MIT License
+"""
+
